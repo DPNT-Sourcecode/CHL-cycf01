80217d49393847bc75360632c433a518788f21a0
sbx_user1051 <sbx_user1051@169.254.33.241>
2019-11-11 18:31:40 +0000

    Mon Nov 11 18:31:40 UTC 2019

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,4 +1,18 @@
 package befaster.solutions.HLO;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HelloSolutionTest {
+
+    private HelloSolution sut = new HelloSolution();
+
+    @Test
+    public void shouldSayHelloWorld() {
+        String friendName = "Luis";
+        String returnedMessage = sut.hello(friendName);
+        assertEquals("Hello, Luis", returnedMessage);
+    }
 }
+
