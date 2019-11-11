e8ee3ad7f1497b217463dc42b3849a8027947670
sbx_user1051 <sbx_user1051@169.254.33.241>
2019-11-11 21:14:48 +0000

    Mon Nov 11 21:14:48 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -11,8 +11,8 @@ public class CheckliteSolution {
     private static Item ITEM_D = new Item('D', 15);
     //private static Item ITEM_E = new Item('E', 40);
 
-    private static SpecialOffer SPECIAL_OFFER_ITEM_A = new SpecialOffer(3, 130);
-    //private static SpecialOffer SPECIAL_OFFER_ITEM_A_2 = new SpecialOffer(5, 200);
+    private static SpecialOffer SPECIAL_OFFER_ITEM_A_1 = new SpecialOffer(3, 130);
+    private static SpecialOffer SPECIAL_OFFER_ITEM_A_2 = new SpecialOffer(5, 200);
     private static SpecialOffer SPECIAL_OFFER_ITEM_B = new SpecialOffer(2, 45);
 
     private Map<Character, Item> catalog;
@@ -28,7 +28,8 @@ public class CheckliteSolution {
         catalog.put(ITEM_C.getSku(), ITEM_C);
         catalog.put(ITEM_D.getSku(), ITEM_D);
 
-        ITEM_A.addSpecialOffer(SPECIAL_OFFER_ITEM_A);
+        ITEM_A.addSpecialOffer(SPECIAL_OFFER_ITEM_A_1);
+        ITEM_A.addSpecialOffer(SPECIAL_OFFER_ITEM_A_2);
         ITEM_B.addSpecialOffer(SPECIAL_OFFER_ITEM_B);
     }
 
@@ -40,7 +41,7 @@ public class CheckliteSolution {
         Integer total = 0;
         Map<Character, Integer> quantityBySku = new HashMap<>();
 
-        
+
 
 
         for (int i = 0; i < skus.length(); i++) {
@@ -94,6 +95,7 @@ public class CheckliteSolution {
         return priceWithoutDiscount - specialOffer.getPrice();
     }
 }
+
 
 
 
