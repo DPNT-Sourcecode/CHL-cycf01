01b01b6ab074dd2d1e46b1e2b760221b2e470b79
sbx_user1051 <sbx_user1051@169.254.33.241>
2019-11-11 23:56:49 +0000

    Mon Nov 11 23:56:49 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -66,7 +66,7 @@ public class CheckliteSolution {
         }
 
         // Apply offers and calculate total
-
+        
 
         Integer total = 0;
         Iterator<Map.Entry<Item, Integer>> iterator = quantityByItem.entrySet().iterator();
@@ -118,5 +118,6 @@ public class CheckliteSolution {
         return null;
     }
 }
+
 
 
