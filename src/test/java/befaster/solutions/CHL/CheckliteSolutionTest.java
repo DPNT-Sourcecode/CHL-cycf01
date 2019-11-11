811dd0693164b2ee92a903b92847c310be3fb155
sbx_user1051 <sbx_user1051@169.254.33.241>
2019-11-11 21:44:48 +0000

    Mon Nov 11 21:44:48 UTC 2019

--- src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
+++ src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -12,7 +12,7 @@ import java.util.Collection;
 @RunWith(Parameterized.class)
 public class CheckliteSolutionTest {
 
-    @Parameterized.Parameters
+    @Parameterized.Parameters(name = "Test with items [{0}] should result in a total of {1}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
                 {"", Integer.valueOf(0)},
@@ -47,73 +47,8 @@ public class CheckliteSolutionTest {
 
         Assert.assertEquals(expectedTotal, checkoutValue);
     }
-
-    /*
-    @Test
-    public void shouldCalculateTotalWithItemInBasket() {
-        String basket = "A";
-        Integer checkoutValue = checkliteSolution.checklite(basket);
-
-        Assert.assertEquals(Integer.valueOf(50), checkoutValue);
-    }
-
-    @Test
-    public void shouldCalculateTotalWithOneItemInBasket() {
-        String basket = "A";
-        Integer checkoutValue = checkliteSolution.checklite(basket);
-
-        Assert.assertEquals(Integer.valueOf(50), checkoutValue);
-    }
-
-    @Test
-    public void shouldCalculateTotalWithMoreThanOneItemInBasket() {
-        String basket = "ABCD";
-        Integer checkoutValue = checkliteSolution.checklite(basket);
-
-        Assert.assertEquals(Integer.valueOf(115), checkoutValue);
-    }
-
-    @Test
-    public void shouldCalculateItemTotalWhenSpecialOfferAppliesToProductA() {
-        String basket = "AAA";
-        Integer checkoutValue = checkliteSolution.checklite(basket);
-
-        Assert.assertEquals(Integer.valueOf(130), checkoutValue);
-    }
-
-    @Test
-    public void shouldCalculateItemTotalWhenSpecialOfferAppliesToProductB() {
-        String basket = "BB";
-        Integer checkoutValue = checkliteSolution.checklite(basket);
-
-        Assert.assertEquals(Integer.valueOf(45), checkoutValue);
-    }
-
-    @Test
-    public void shouldCalculateItemTotalWithMultipleSpecialOffers() {
-        String basket = "AAAAAA";
-        Integer checkoutValue = checkliteSolution.checklite(basket);
-
-        Assert.assertEquals(Integer.valueOf(260), checkoutValue);
-    }
-
-    @Test
-    public void shouldReturnMinusOneIfInputIsNull() {
-        String invalidInput = null;
-        Integer returnedValue = checkliteSolution.checklite(invalidInput);
-
-        Assert.assertEquals(Integer.valueOf(-1), returnedValue);
-    }
-
-    @Test
-    public void shouldReturnMinusOneIfInputContainsInvalidSku() {
-        String invalidInput = "!";
-        Integer returnedValue = checkliteSolution.checklite(invalidInput);
-
-        Assert.assertEquals(Integer.valueOf(-1), returnedValue);
-    }
-    */
 }
+
 
 
 
