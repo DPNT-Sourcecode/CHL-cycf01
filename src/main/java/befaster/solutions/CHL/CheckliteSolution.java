2bcebbcea403f7c3f2527897764ba43b957e6ebc
sbx_user1051 <sbx_user1051@169.254.33.241>
2019-11-11 22:38:49 +0000

    Mon Nov 11 22:38:49 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -12,7 +12,7 @@ public class CheckliteSolution {
     private static Item ITEM_C = new Item('C', 20);
     private static Item ITEM_D = new Item('D', 15);
     //private static Item ITEM_E = new Item('E', 40);
-    
+
     private static SpecialOffer SPECIAL_OFFER_ITEM_A_1 = new SpecialOffer(5, 200, 1);
     private static SpecialOffer SPECIAL_OFFER_ITEM_A_2 = new SpecialOffer(3, 130, 2);
     private static SpecialOffer SPECIAL_OFFER_ITEM_B = new SpecialOffer(2, 45);
@@ -123,6 +123,7 @@ public class CheckliteSolution {
         return null;
     }
 }
+
 
 
 
