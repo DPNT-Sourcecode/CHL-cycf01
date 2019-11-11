58b95ce0d574192d9a819cee1850983aa1b0f3bd
sbx_user1051 <sbx_user1051@169.254.33.241>
2019-11-11 19:28:03 +0000

    Mon Nov 11 19:28:03 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -2,14 +2,29 @@ package befaster.solutions.CHL;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.Set;
 
 public class CheckliteSolution {
+
+    private static Item ITEM_A = new Item("A", BigDecimal.valueOf(50));
+    private static Item ITEM_B = new Item("B", BigDecimal.valueOf(30));
+    private static Item ITEM_C = new Item("C", BigDecimal.valueOf(20));
+    private static Item ITEM_D = new Item("D", BigDecimal.valueOf(15));
 
     private Set<Item> catalog;
 
     public CheckliteSolution() {
         //this.catalog = catalog;
+    }
+
+    private void initCatalog() {
+        catalog = new HashSet<>();
+        catalog.add(ITEM_A);
+        catalog.add(ITEM_B);
+        catalog.add(ITEM_C);
+        catalog.add(ITEM_D);
     }
 
     public Integer checklite(String skus) {
@@ -24,3 +39,4 @@ public class CheckliteSolution {
         return total;
     }
 }
+
