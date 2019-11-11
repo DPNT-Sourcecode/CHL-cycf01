c2013a2eeabb947fe8520303a30b87df51827fa0
sbx_user1051 <sbx_user1051@169.254.33.241>
2019-11-11 18:43:03 +0000

    Mon Nov 11 18:43:03 UTC 2019

---
+++ src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -0,0 +1,19 @@
+package befaster.solutions.CHL;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CheckliteSolutionTest {
+
+    CheckliteSolution checkliteSolution;
+
+    @Before
+    public void setUp() {
+        checkliteSolution = new CheckliteSolution();
+    }
+
+    @Test
+    public void shouldCalculateTotalWithOneItem() {
+
+    }
+}
