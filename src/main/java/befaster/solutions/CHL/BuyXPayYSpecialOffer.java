1d6a9d3b53b66f092a3cfc524b259b39ec6719f1
sbx_user1051 <sbx_user1051@169.254.33.241>
2019-11-11 23:32:49 +0000

    Mon Nov 11 23:32:49 UTC 2019

--- src/main/java/befaster/solutions/CHL/BuyXPayYSpecialOffer.java
+++ src/main/java/befaster/solutions/CHL/BuyXPayYSpecialOffer.java
@@ -2,17 +2,38 @@ package befaster.solutions.CHL;
 
 import java.util.Map;
 
-public class BuyXPayYSpecialOffer extends SpecialOffer {
+public class BuyXPayYSpecialOffer extends SpecialOffer implements Comparable<SpecialOffer> {
     private int quantity;
     private Integer price;
+    private int ranking;
 
-    public BuyXPayYSpecialOffer(int quantity, Integer price) {
-        super(quantity, price);
+    public BuyXPayYSpecialOffer(int quantity, Integer price, int ranking) {
+        this.quantity = quantity;
+        this.price = price;
+        this.ranking = ranking;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public int getRanking() {
+        return ranking;
     }
 
     @Override
     public void apply(Map<Item, Integer> quantityByItem) {
 
     }
+
+    @Override
+    public int compareTo(SpecialOffer other) {
+        return this.ranking - other.ranking;
+    }
 }
+
 
--- src/main/java/befaster/solutions/CHL/SpecialOffer.java
+++ src/main/java/befaster/solutions/CHL/SpecialOffer.java
@@ -5,39 +5,9 @@ import java.util.Map;
 
 public abstract class SpecialOffer implements Comparable<SpecialOffer> {
 
-    private int quantity;
-    private Integer price;
-    private int ranking;
-
-    public SpecialOffer(int quantity, Integer price) {
-        this(quantity, price, 1);
-    }
-
-    public SpecialOffer(int quantity, Integer price, int ranking) {
-        this.quantity = quantity;
-        this.price = price;
-        this.ranking = ranking;
-    }
-
     public abstract void apply(Map<Item, Integer> quantityByItem);
 
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public int getRanking() {
-        return ranking;
-    }
-
-    @Override
-    public int compareTo(SpecialOffer other) {
-        return this.ranking - other.ranking;
-    }
-
 }
+
 
 
